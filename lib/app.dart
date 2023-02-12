a0211aacd3325145913e60d0932e9b7207960681
DongHyun Kim <temeddix@gmail.com>
2023-02-12 21:50:41 +0900

    Fix API code in Dart

--- lib/app.dart
+++ lib/app.dart
@@ -4,9 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_window_close/flutter_window_close.dart';
 import 'state.dart';
-import 'bridge/first_crate/ffi.dart' as first_crate;
-import 'bridge/second_crate/ffi.dart' as second_crate;
 import 'value.dart';
+import 'bridge/ffi.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -96,8 +95,7 @@ class HomePage extends StatelessWidget {
           context.read<AppState>().setState((AppState s) async {
             int original = s.tester.counterValue;
             int calculated = original;
-            calculated = await second_crate.api.multiplyTwo(before: calculated);
-            calculated = await first_crate.api.addOne(before: calculated);
+            calculated = await api.addThree(before: calculated);
             s.tester.counterValue = calculated;
           });
         },
